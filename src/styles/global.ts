351510cc9ead148fdbbda764af4516842973ff93
muriloignaccio <murilohsignacio@gmail.com>
2021-06-03 01:41:56 -0300

    fix sintax issue

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,15 +3,15 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #F0F2F5;
-    --red: #E52E4D
-    --blue: #5429CC
+    --red: #E52E4D;
+    --blue: #5429CC;
 
-    --blue-light: #6933FF
+    --blue-light: #6933FF;
 
-    --text-title: #363F5F
-    --text-body: #969CB3
+    --text-title: #363F5F;
+    --text-body: #969CB3;
 
-    -shape: #FFFFFF
+    --shape: #FFFFFF;
   }
 
   * {
