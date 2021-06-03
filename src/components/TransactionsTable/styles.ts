dc192f535dd0c0b4e150d3eba57f9f1f1afb5a0b
muriloignaccio <murilohsignacio@gmail.com>
2021-06-03 02:44:15 -0300

    Create transactions table component

---
+++ src/components/TransactionsTable/styles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin-top: 4rem;
+
+  table {
+    width: 100%;
+    border-spacing: 0 .5rem;
+
+    th {
+      color: var(--text-body);
+      font-weight: 400;
+      padding: 1rem 2rem;
+      text-align: left;
+      line-height: 1.5rem;
+    }
+
+    td {
+      background: var(--shape);
+      border: none;
+      border-radius: .25rem;
+      padding: 1rem 2rem;
+
+      color: var(--text-body);
+
+      &:first-child {
+        color: var(--text-title);
+      }
+
+      &.deposit {
+        color: var(--green);
+      }
+
+      &.withdraw {
+        color: var(--red);
+      }
+    }
+  }
+`;
