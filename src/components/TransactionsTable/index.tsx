eaa0753497d176f7a602dc8d59fc3f7e9e7efef3
muriloignaccio <murilohsignacio@gmail.com>
2021-06-04 01:19:32 -0300

    Implemented the api in all transaction rendering

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,12 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 import { api } from "../../services/api";
+
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number;
+  title: string;
+  type: string;
+  category: string;
+  amount: number;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
-  
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
   useEffect(() => {
-    api.get('http://localhost:3000/api/transactions')
-      .then(({ data }) => console.log(data));
+    api
+      .get("http://localhost:3000/api/transactions")
+      .then(({ data }) => setTransactions(data.transactions));
   }, []);
 
   return (
@@ -22,33 +35,23 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de site</td>
-            <td className="deposit">R$ 12.000,00</td>
-            <td>Venda</td>
-            <td>13/04/2021</td>
-          </tr>
-
-          <tr>
-            <td>Hamburguer</td>
-            <td className="withdraw">- R$ 59,00</td>
-            <td>Alimentação</td>
-            <td>10/04/2021</td>
-          </tr>
-
-          <tr>
-            <td>Aluguel de apartamento</td>
-            <td className="withdraw">- R$ 1.200,00</td>
-            <td>Casa</td>
-            <td>27/03/2021</td>
-          </tr>
-
-          <tr>
-            <td>Computador</td>
-            <td className="deposit">R$ 5.400,00</td>
-            <td>Venda</td>
-            <td>15/03/2021</td>
-          </tr>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat("pt-BR").format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createServer, Model } from 'miragejs';
-import { App } from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createServer, Model } from "miragejs";
+import { App } from "./App";
 
 createServer({
   models: {
-    transaction: Model
+    transaction: Model,
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Desenvolvimento de site",
+          amount: 12000,
+          type: "deposit",
+          category: "Venda",
+          createdAt: new Date("2021-04-13 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Hamburguer",
+          amount: -59,
+          type: "withdraw",
+          category: "Alimentação",
+          createdAt: new Date("2021-04-10 09:00:00"),
+        },
+        {
+          id: 3,
+          title: "Aluguel de apartamento",
+          amount: -1200,
+          type: "withdraw",
+          category: "Casa",
+          createdAt: new Date("2021-03-27 09:00:00"),
+        },
+        {
+          id: 4,
+          title: "Computador",
+          amount: 5400,
+          type: "deposit",
+          category: "Venda",
+          createdAt: new Date("2021-03-15 09:00:00"),
+        },
+      ],
+    });
   },
   routes() {
-    this.namespace = 'api';
+    this.namespace = "api";
 
-    this.get('/transactions', () => {
-      return this.schema.all('transaction');
+    this.get("/transactions", () => {
+      return this.schema.all("transaction");
     });
 
-    this.post('/transactions', (schema, request) => {
+    this.post("/transactions", (schema, request) => {
       const data = JSON.parse(request.requestBody);
 
-      return schema.create('transaction', data);
+      return schema.create("transaction", data);
     });
-  }
+  },
 });
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
